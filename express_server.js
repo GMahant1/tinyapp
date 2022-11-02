f372a74f3f5544ad7f5fc330c0c453f11f1913c4
Gaurav Mahant <thegau22@gmail.com>
2022-11-02 01:28:47 -0600

    add app.post for  when browser post data to server, declare random string generator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const crypto = require("crypto");
+function generateRandomString () {
+  let id = crypto.randomBytes(3).toString("hex");
+  return id;
+};
+
 
 app.set("view engine", "ejs");
 
@@ -8,6 +14,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,6 +30,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id : req.params.id, longURL : urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -33,6 +45,11 @@ app.get("/u/:id", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
