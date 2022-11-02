8f834f43abcd67d82a5151d0440703265305a3d9
Gaurav Mahant <thegau22@gmail.com>
2022-11-02 02:45:06 -0600

    save unique id and longURL into database and redirect properly

--- express_server.js
+++ express_server.js
@@ -49,7 +49,9 @@ app.get("/hello", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
 });
 
 app.listen(PORT, () => {
