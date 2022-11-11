98ab0378bebdc239de972b6c22c1d7153292b78f
Gaurav Mahant <thegau22@gmail.com>
2022-11-11 03:35:09 -0700

    add password hash function, implement when creating user in /register and check in /login

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const { url } = require("inspector");
 function generateRandomString() {
@@ -70,6 +71,11 @@ const urlsForUser = function(id) {
   return null;
 };
 
+//function to hash password
+const hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -108,7 +114,7 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const cookieValue = req.cookies["user_id"];
   const user = users[cookieValue];
-  const templateVars = { user }
+  const templateVars = { user };
   if (userLoggedIn(req)) {
     return res.render("urls_new", templateVars);
   }
@@ -126,7 +132,7 @@ app.get("/urls/:id", (req, res) => {
     return res.status(403).send("Please log in to view shortened URLS!");
   };
   if (urlsForUser(cookieValue) === null) {
-    return res.status(403).send("This does not belong to you!")
+    return res.status(403).send("This does not belong to you!");
   } else {
     return res.render("urls_show", templateVars);
   }
@@ -140,9 +146,9 @@ app.get("/register", (req, res) => {
   //check if user is logged in, show /urls otherwise ask to register with "user_registration"
   if (userLoggedIn(req)) {
     return res.redirect("/urls");
-  } 
+  }
   res.render("user_registration", templateVars);
-})
+});
 
 //checked (console prints an error when i input an id that does not exist)
 app.get("/u/:id", (req, res) => {
@@ -155,7 +161,7 @@ app.get("/u/:id", (req, res) => {
 //checked
 app.get("/login", (req, res) => {
   //varaible set to cookie object containg key(name of cookie) and value (id)
-  const cookieValue = req.cookies["user_id"]
+  const cookieValue = req.cookies["user_id"];
   //pass variable (cookie name) into users object to access user information
   const user = users[cookieValue];
   //pass user object to display email when logged in 
@@ -165,7 +171,7 @@ app.get("/login", (req, res) => {
     return res.redirect("/urls");
   }
   res.render("user_login", templateVars);
-})
+});
 
 //checked using urls.json after creation
 app.post("/urls", (req, res) => {
@@ -175,10 +181,10 @@ app.post("/urls", (req, res) => {
     const newURL = {
       longURL: req.body.longURL,
       userID: req.cookies.user_id
-    }
+    };
     urlDatabase[id] = newURL;
     return res.redirect(`/urls/${id}`);
-  } 
+  }
   res.status(403).send("Please log in to shorten the URl!");
 });
 
@@ -191,7 +197,7 @@ app.post("/urls/:id/delete", (req, res) => {
     return res.status(403).send("Please log in to delete shortened URLS!");
   };
   if (urlsForUser(cookieValue) === null) {
-    return res.status(403).send("This does not belong to you!")
+    return res.status(403).send("This does not belong to you!");
   } else {
     delete urlDatabase[req.params.id];
     res.redirect("/urls");
@@ -207,7 +213,7 @@ app.post("/urls/:id/edit", (req, res) => {
     return res.status(403).send("Please log in to edit shortened URLS!");
   };
   if (urlsForUser(cookieValue) === null) {
-    return res.status(403).send("This does not belong to you!")
+    return res.status(403).send("This does not belong to you!");
   } else {
     urlDatabase[req.params.id].longURL = req.body.longURL;
     return res.redirect("/urls");
@@ -216,34 +222,36 @@ app.post("/urls/:id/edit", (req, res) => {
 
 //checked
 app.post("/login", (req, res) => {
+  const hashedPassword = hashPassword(req.body.password);
   let userInfo = userExist(req.body.email);
   if (userInfo === null) {
     res.sendStatus(403);
   }
-  if (userInfo.password !== req.body.password) {
+  if (!bcrypt.compareSync(userInfo.password, hashedPassword)) {
     res.sendStatus(403);
   } else {
     res.cookie("user_id", userInfo.id);
     res.redirect("/urls");
   }
-})
+});
 
 //checked
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/login");
-})
+});
 
 //checked
 app.post("/register", (req, res) => {
   //console.log(req.body);
+  const hashedPassword = hashPassword(req.body.password);
   if (req.body.email === "" || req.body.password === "") {
     res.sendStatus(404);
   } if (userExist(req.body.email) !== null) {
     res.sendStatus(404);
   } else {
     const id = generateRandomString();
-    users[id] = {id, email: req.body.email, password: req.body.password } ; 
+    users[id] = { id, email: req.body.email, password: hashedPassword };
     res.cookie("user_id", id);
     //console.log(users);
     res.redirect("/urls");
