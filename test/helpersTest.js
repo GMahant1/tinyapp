8691f24200955a68b4db90764688ce97ae1229de
Gaurav Mahant <thegau22@gmail.com>
2022-11-11 13:08:38 -0700

    write mocha test and ensure they pass

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,26 +4,26 @@ const { userExist } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('userExist', () => {
   it('should return a user with valid email', () => {
-    const user = userExist("user@example.com", testUsers)
+    const user = userExist("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(user.id, expectedUserID);
   });
 
-it('should return null with non-existant email', () => {
-  const user = userExist('test@example.com', testUsers);
-  assert.strictEqual(user, null);
-});
+  it('should return null with non-existant email', () => {
+    const user = userExist('test@example.com', testUsers);
+    assert.strictEqual(user, null);
+  });
 });
